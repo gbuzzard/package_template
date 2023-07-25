d27bd0b687e73da53b64f2302fd64464faffb3a3
Greg Buzzard <buzzard@purdue.edu>
2023-07-25 12:14:43 -0400

    Improve download of data.

--- {{cookiecutter.project_name}}/dev_scripts/get_demo_data_server.sh
+++ {{cookiecutter.project_name}}/dev_scripts/get_demo_data_server.sh
@@ -10,24 +10,27 @@ GPUCLUSTER="gilbreth"
 CPUCLUSTER="brown"
 
 source1="/depot/bouman/users/nhalavic/pcd_data"
-target1="."
 source2="/depot/bouman/users/mnagare/calibration_module_demo_data"
-target2="../../data"
 
-# get data
-# assuming current working dir is {{cookiecutter.project_name}}/dev_scripts
+# Note that the target directories are relative to dev_scripts
+target_dir1="../demo/data"
+target_dir2="../data"
 
-cd ../demo
-mkdir -p data
-cd data
+mkdir -p "$target_dir1"
+mkdir -p "$target_dir2"
+
+red=`tput setaf 1`
+green=`tput setaf 2`
+reset=`tput sgr0`
 
 if [[ "$HOSTNAME" == *"$GPUCLUSTER"* || "$HOSTNAME" == *"$CPUCLUSTER"* ]]; then
-  # cp -r $source1 $target1
-  # cp -r $source2 $target2
+    echo "${red}   Edit get_demo_data_server.sh to get correct data.    ${reset}"
+    # cp -r $source1 $target1
+    # cp -r $source2 $target2
 else
-  # scp -r $cluster_user_name@gilbreth.rcac.purdue.edu:$source1 $target1
-  # scp -r $cluster_user_name@gilbreth.rcac.purdue.edu:$source2 $target2
+    echo "${red}   Edit get_demo_data_server.sh to get correct data.    ${reset}"
+    # scp -r $cluster_user_name@gilbreth.rcac.purdue.edu:$source1 $target1
+    # scp -r $cluster_user_name@gilbreth.rcac.purdue.edu:$source2 $target2
 fi
 
-cd ../../dev_scripts
 
