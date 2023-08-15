7022150c367228e78c1518c39454b676b15f557d
Greg Buzzard <buzzard@purdue.edu>
2023-08-15 14:24:47 -0400

    Update name and signature of imshow_hi_res.

--- {{cookiecutter.project_name}}/demo/demo_utils.py
+++ {{cookiecutter.project_name}}/demo/demo_utils.py
@@ -2,19 +2,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def imshow_array(array, title='', vmin=None, vmax=None, show=True):
+def imshow_hi_res(array, title='', vmin=None, vmax=None, cmap='viridis', show=False):
     """ Display an array using a figure size large enough to map one array element to one pixel
     The scaling isn't exactly correct, so some pixels are slightly larger than others, partially depending on the
     labels on the axes and on the colorbar.  It's difficult to get this exactly correct.
     Args:
         array: The array to display
         title: The title for the plot
-        vmin: Same as vmin in imshow
-        vmax: Same as vmax in imshow
-        show: If true, then plt.show() is called to display immediately.
+        vmin: Minimum of the intensity window - same as vmin in imshow
+        vmax: Maximum of the intensity window - same as vmax in imshow
+        cmap: The color map as in imshow - same as cmap in imshow
+        show: If true, then plt.show() is called to display immediately, otherwise call
+              fig.show() on the object returned from this function to show the plot.
 
     Returns:
-        Nothing
+        The pyplot figure object
     """
     px = 1 / plt.rcParams['figure.dpi']  # pixel in inches
     # Scale for one pixel per element and account for the border
@@ -22,12 +24,14 @@ def imshow_array(array, title='', vmin=None, vmax=None, show=True):
     fig_width = 4.6 * array.shape[1] * px
     fontsize = max([fig_height, 6.0])
     plt.rcParams.update({'font.size': fontsize})
-    plt.figure(figsize=(fig_width, fig_height), layout="constrained")
-    plt.imshow(array, vmin=vmin, vmax=vmax, interpolation='none')
+
+    fig = plt.figure(figsize=(fig_width, fig_height), layout="constrained")
+    plt.imshow(array, vmin=vmin, vmax=vmax, cmap=cmap, interpolation='none')
     plt.colorbar()
     plt.title(title)
     if show:
         plt.show()
+    return fig
 
 
 def have_same_shape(array1, array2):
