03614fe551582149cea786b18bcb0f2207ca354b
gbuzzard <54102356+gbuzzard@users.noreply.github.com>
2024-02-12 09:58:02 -0500

    Update the use of imshow.  The hi_res version is not needed with native python plots since they allow for zoom in.

--- {{cookiecutter.project_name}}/demo/demo_utils.py
+++ {{cookiecutter.project_name}}/demo/demo_utils.py
@@ -2,10 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def imshow_hi_res(array, title='', vmin=None, vmax=None, cmap='viridis', show=False):
-    """ Display an array using a figure size large enough to map one array element to one pixel
-    The scaling isn't exactly correct, so some pixels are slightly larger than others, partially depending on the
-    labels on the axes and on the colorbar.  It's difficult to get this exactly correct.
+def imshow_with_options(array, title='', vmin=None, vmax=None, cmap='viridis', show=False):
+    """ Display an array as an image along with optional title and intensity window.
     Args:
         array: The array to display
         title: The title for the plot
@@ -18,14 +16,7 @@ def imshow_hi_res(array, title='', vmin=None, vmax=None, cmap='viridis', show=Fa
     Returns:
         The pyplot figure object
     """
-    px = 1 / plt.rcParams['figure.dpi']  # pixel in inches
-    # Scale for one pixel per element and account for the border
-    fig_height = 3.7 * array.shape[0] * px
-    fig_width = 4.6 * array.shape[1] * px
-    fontsize = max([fig_height, 6.0])
-    plt.rcParams.update({'font.size': fontsize})
-
-    fig = plt.figure(figsize=(fig_width, fig_height), layout="constrained")
+    fig = plt.figure(layout="constrained")
     plt.imshow(array, vmin=vmin, vmax=vmax, cmap=cmap, interpolation='none')
     plt.colorbar()
     plt.title(title)
@@ -33,9 +24,3 @@ def imshow_hi_res(array, title='', vmin=None, vmax=None, cmap='viridis', show=Fa
         plt.show()
     return fig
 
-
-def have_same_shape(array1, array2):
-    """
-    Check if two arrays have the same shape - return True if so, false if not.
-    """
-    return np.allclose(np.array(array1.shape), np.array(array2.shape))
